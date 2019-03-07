6202fdd9447110942c4f6d31ee49babacfea9e66
notshock <wenshock@gmail.com>
2019-03-07 10:30:33 +0800

    :han:

--- src/com/biglottorecord/controller/BigLottoRecordServlet.java
+++ src/com/biglottorecord/controller/BigLottoRecordServlet.java
@@ -7,9 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class BigLottoRecordServlet
- */
+/
 @WebServlet("/BigLottoRecordServlet")
 public class BigLottoRecordServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -19,18 +17,12 @@ public class BigLottoRecordServlet extends HttpServlet {
         super();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		request.setCharacterEncoding("UTF-8");
 		
 		
 	}
